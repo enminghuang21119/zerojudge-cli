13db90a8e66e3cda02fa755dd62ff50ab9b3ad80
Choder <choder69864501@gmail.com>
2018-07-20 15:17:33 +0800

    build to exe

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from cx_Freeze import setup,Executable
 base=''
 if sys.platform=='win32':
     base="Win32GUI"
+packages=["idna","requests","multiprocessing"]
+options={'build_exe':{'packages':packages,},}
 setup(
     name='zerojudge.exe',
-    version='3.6.6',
+    version='0.1',
+    options=options,
     description='zerojudge in command line',
     executables=[Executable("zerojudge-cli.py",base=base)]
 )
