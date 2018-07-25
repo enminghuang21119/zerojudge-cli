ceaff02c2421ca33d959ec5588eba8967877b838
Choder <choder69864501@gmail.com>
2018-07-25 11:13:29 +0800

    pep8 style

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 from cx_Freeze import setup, Executable
 
-base = None    
+base = None
 
 executables = [Executable("zerojudge-cli.py", base=base)]
 
 # packages = ["idna"]
 options = {
-    'build_exe': {    
-        'packages':['bs4', 'getpass', 'lxml', 'webbrowser', 'requests', 'colorTable', 'queue', 'idna', 'sys', 'colorama'],
-    },    
+    'build_exe': {
+        'packages': [
+            'bs4',
+            'getpass',
+            'lxml',
+            'webbrowser',
+            'requests',
+            'colorTable',
+            'queue',
+            'idna',
+            'sys',
+            'colorama'],
+    },
 }
 
 setup(
-    name = "zerojudge-cli",
-    options = options,
-    version = "1.0",
-    description = 'Build by samuel21119',
-    executables = executables
+    name="zerojudge-cli",
+    options=options,
+    version="1.0",
+    description='Build by samuel21119 and Choder',
+    executables=executables
 )
